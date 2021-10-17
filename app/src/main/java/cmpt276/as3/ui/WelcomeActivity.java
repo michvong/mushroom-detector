eb9160470b4a2b094721047df84036ab705a8355
mva30 <mva30@sfu.ca>
2021-10-17 02:57:57 -0700

    Fix main menu transition.
    Add finish() to openMainActivity to prevent main menu random duplicate transition.

--- app/src/main/java/cmpt276/as3/ui/WelcomeActivity.java
+++ app/src/main/java/cmpt276/as3/ui/WelcomeActivity.java
@@ -37,6 +37,7 @@ public class WelcomeActivity extends AppCompatActivity {
     public void openMainActivity() {
         Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
 }
